a496dfcfb2010accc40bccccef7bf908bfc64708
AlanDeveloper <alanssantos32@gmail.com>
2018-08-21 08:49:35 -0300

    Update script.js

--- Stars/js/script.js
+++ Stars/js/script.js
@@ -50,7 +50,7 @@ function create() {
     ledge.body.immovable = true;
 
     ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = false;
+    ledge.body.immovable = true;
 
     // Adicionando player
     player = game.add.sprite(32, game.world.height - 150, 'dude');
