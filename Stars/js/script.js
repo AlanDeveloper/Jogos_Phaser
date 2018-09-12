4925633ebc7fc989b669a9b170d18a87371482bc
AlanDeveloper <alanssantos32@gmail.com>
2018-09-12 09:42:38 -0300

    Update script.js

--- Stars/js/script.js
+++ Stars/js/script.js
@@ -87,7 +87,7 @@ function create() {
     }
 
     //  Score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
 
     // Controles 
     cursors = game.input.keyboard.createCursorKeys();
